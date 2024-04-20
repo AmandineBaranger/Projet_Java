092ca1ccc8d9de8732fec0114d6be3729ff4754b
AmandineBaranger <amande.baranger@gmail.com>
2024-04-20 14:29:41 +0200

    init

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import re
 import tabulate
 from tabulate import tabulate
+#from pydantic import BaseModel
 
+# Define classes for Alphabet, State, and Transition
 class Alphabet:
     def __init__(self, word):
         self.word = word 
@@ -22,45 +24,53 @@ class Transition:
     def __repr__(self): 
         return f"Transition({self.from_state}->{self.word}->{self.to_state})"
 
-        
+# Define class for FiniteAutomaton
 class FiniteAutomate:
     def __init__(self,name):
         self.name = name 
-        self.words = []
+        self.alphabet = []
         self.states = []
         self.transitions = []
         self.initial_states = []
         self.final_states = []
         
     def add_state(self, state):
+        """Add a state to the automaton."""
         self.states.append(state)
     
-    def get_state_from(self, state_name):
+    def get_state_from_name(self, state_name):
+        """Get a state from its name."""
         for state in self.states:
             if state.name == state_name:
                 return state
+        raise Exception('Sorry, no state with this name')
         return None
     
-    def is_in_inital_states(self, state_name) -> bool:
+    def is_in_inital_states(self, initial_state) -> bool:
+        """Check if a state is an initial state."""
+        if not isinstance(initial_state, State):
+            raise TypeError('Error: must be a State')
         for state in self.initial_states:
-            if state is state_name:
+            if state is initial_state:
                 return True
         return False
     
     def is_in_final_states(self, state_name) -> bool:
+        """Check if a state is a final state."""
         for state in self.final_states:
             if state is state_name:
                 return True
         return False
     
     def get_word_from(self, word_name):
-        for word in self.words:
+        """Get a word from its name."""
+        for word in self.alphabet:
             if word.word == word_name:
                 return word
         return None
     
     def is_standard(self) -> bool:
-        #TODO to complete/modify
+        """Check if the automaton is in standard form."""
         if len(self.initial_states) == 1:
             # Check if no transition is going to initial state
             initial_state = self.initial_states[0]
@@ -72,18 +82,37 @@ class FiniteAutomate:
         else:
             return False
         
-    # TODO : à implémenter
     def is_deterministic(self) -> bool:
+        """Check if the automaton is deterministic."""
+         # Verifies for each state and each symbols of the alphabet
+        for state in self.states:
+            for word in self.alphabet:
+                # Count the number of transitions for the current state and the current alphabet
+                transitions_count = sum(
+                    1 for transition in self.transitions if transition.from_state == state and transition.word == word)
+                # If the number of transitions is greater than 1, then this automaton is not deterministic
+                if transitions_count > 1:
+                    return False
         return True
 
-    # TODO : à implémenter
     def is_complete(self) -> bool:
+        """Check if the automaton is complete."""
+         # Verifies for each states and each symbols of the alphabet
+        for state in self.states:
+            for word in self.alphabet:
+                # Search a corresponding transition
+                transition_found = any(transition.from_state == state and transition.word == word for transition in self.transitions)
+                # If no transition is found, then the automata is not complete
+                if not transition_found:
+                    return False
         return True
     
     def add_word(self, word):
-        self.words.append(word)
+        """Add a word to the alphabet."""
+        self.alphabet.append(word)
         
     def read_from_file(self,filename):
+        """Read data from a file and initialize the automaton."""
         with open(filename, 'r') as file:
 
             # Read and generate words
@@ -103,7 +132,7 @@ class FiniteAutomate:
             num_initial_states = int(initial_states_info[0])
             initial_states = set(initial_states_info[1:num_initial_states + 1])
             for state_name in initial_states :
-                self.initial_states.append(self.get_state_from(state_name))    
+                self.initial_states.append(self.get_state_from_name(state_name))    
             #print(initial_states)
             
             # Read final states
@@ -111,7 +140,7 @@ class FiniteAutomate:
             num_final_states = int(final_states_info[0])
             final_states = set(final_states_info[1:num_final_states + 1])
             for state_name in final_states :
-                self.final_states.append(self.get_state_from(state_name))    
+                self.final_states.append(self.get_state_from_name(state_name))    
             #print(final_states)
 
             # Read transitions
@@ -126,89 +155,69 @@ class FiniteAutomate:
                 source_state = parts[0]
                 word = parts[1]
                 ending_state = parts[2]
-                self.transitions.append(Transition(self.get_state_from(source_state),self.get_state_from(ending_state),self.get_word_from(word)))
+                self.transitions.append(Transition(self.get_state_from_name(source_state),self.get_state_from_name(ending_state),self.get_word_from(word)))
                 #print(f"Transistion {source_state}-{word}-{ending_state}")
-            
     
+    # Display the automaton
     def __repr__(self):
-        return f"FA(\n   name={self.name},\n   words={self.words},\n   states={self.states},\n   initial_states={self.initial_states}),\n   final_states={self.final_states}),\n   transition={self.transitions})"
+        return f"FA(\n   name={self.name},\n   alphabet={self.alphabet},\n   states={self.states},\n   initial_states={self.initial_states}),\n   final_states={self.final_states}),\n   transition={self.transitions})"
 
 
-"""def display_finite_automate(fa):
-    print("Finite Automate name:", fa.name)
-    print("Alphabet:", fa.words)
-    print("States:", fa.states)
-    print("Initial states:", fa.initial_states)
-    print("Final states:", fa.final_states)
-    print("Transitions:", fa.transitions)
+# Function to display the automaton
+def display_FA (fa):
+    """Display the automaton."""
+    header = []
+    header.append(" ")
+    header.append(" ")
+    for word in fa.alphabet:
+        header.append(word.word)
+
+    table = [header]
+
+    for state in fa.states:
+        row = []
+        if fa.is_in_inital_states(state):
+            row.append("->")
+        elif fa.is_in_final_states(state):
+            row.append("<-")
+        else:
+            row.append(" ")
         
+        row.append(state.name)
+
+        for _ in range(len(fa.alphabet)):
+            row.append("--")         
+        table.append(row)
+
+    for transition in fa.transitions:
+        source = int(transition.from_state.name)+1
+        dest = int(transition.to_state.name)
+        word = ord(transition.word.word)-95
+        if table[source][word]=="--":
+            table[source][word]=dest
+        else:
+            table[source][word]=f"{table[source][word]},{dest}"
+    print(tabulate(table, tablefmt="grid"))
 
 
-def display_finite_automate_amandine(fa):
-    print("Finite Automate name:", fa.name)
-    print("Alphabet:", fa.words)
-    print("States:", fa.states)"""
-
-def diplay_FA(FA):
-    return None
-    #TODO print the automaton
-
-def standardization(FA) :
-    return None
-    #TODO return the standardize automaton
-
-def completion(FA):
-    #TODO the completion of the automaton
-    return None
-
-def determinization_and_completion_automaton (FA) :
-    #TODO return the determinize and complete automaton
-    return None
-
-def display_complete_dererministic_automaton(CDFA) :
-    #TODO print the CDFA
-    return None
-
-def minimization(CDFA) :
-    #TODO return the minimized automaton
-    return None
-
-def display_minimal_automaton(MCDFA) : 
-    #TODO print the MCDFA
-    return None
-
+# Create the FiniteAutomate object and read data from file
 fa = FiniteAutomate("Test")
-fa.read_from_file("Inputs/Int3-2-20.txt")
-#fa.read_from_file("Inputs/Int3-2-4.txt")
-#display_finite_automate(fa)
-fa.is_standard()
+fa.read_from_file("Inputs/Int3-2-3.txt")
 
-# first row of table
-header = []
-header.append(" ")
-header.append(" ")
-for word in fa.words:
-    header.append(word.word)
+# Display the automaton
+display_FA(fa)
 
-table = [header]
+if fa.is_standard():
+    print("The automaton is standard")
+else:
+    print("The automaton is not standard")
 
+if fa.is_complete():
+    print("The automaton is complete")
+else:
+    print("The automaton is not complete")
 
-for state in fa.states:
-    row = []
-    if fa.is_in_inital_states(state):
-        row.append("->")
-    elif fa.is_in_final_states(state):
-        row.append("<-")
-    else:
-        row.append(" ")
-    row.append(state.name)
-    row.append("--")
-    row.append("--")
-    row.append("--")       
-    table.append(row)
-        
-#table = [header]
-#table = [["Sun",696000,1989100000],["Earth",6371,5973.6],
-#    ["Moon",1737,73.5],["Mars",3390,641.85]]
-
-print(tabulate(table, tablefmt="rounded_grid"))
+if fa.is_deterministic():
+    print("The automaton is not deterministic")
+else:
+    print("The automaton is deterministic")
